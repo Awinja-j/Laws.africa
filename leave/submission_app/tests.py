3012078c0f72e1cd266541942b00dccbed8589ef
Awinja-j <ingari61@hotmail.com>
2022-02-08 23:24:08 +0300

    second commit

--- leave/submission_app/tests.py
+++ leave/submission_app/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+def test_list_leave_requests():
+    
+    assert True
+
+def test_submit_leave_request():
+    assert True 
+
+def test_edit_exsiting_leave_request():
+    assert True
+
