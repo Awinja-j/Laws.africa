2d0cdb630a108641dea3e223312060e465a3adf2
Awinja-j <ingari61@hotmail.com>
2022-02-09 02:06:04 +0300

    working tests and initial migration

--- leave/submission_app/models.py
+++ leave/submission_app/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from rest_framework import serializers
 
-# Create your models here.
+class Leave(models.Model):
+    """
+    Model representing a leave request.
+    """
+    leave_requester = models.TextField(max_length=200)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    reason = models.TextField()
+    status = models.CharField(max_length=1, choices=[('P', 'Pending'), ('A', 'Approved'), ('R', 'Rejected')])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class LeaveSerializer(serializers.Serializer):
+
+        """
+        Serialize Leave object
+        """
+        leave_requester = serializers.CharField()
+        start_date = serializers.DateField()
+        end_date = serializers.DateField()
+        reason = serializers.CharField()
+        status = serializers.ChoiceField(choices=[('P', 'Pending'), ('A', 'Approved'), ('R', 'Rejected')])
+        created_at = serializers.DateTimeField()
+        updated_at = serializers.DateTimeField()
+
+        class Meta:
+            model = 'Leave'
+            fields = '__all__'
+
+        def create(self, validated_data):
+            """
+            Create and return a new `Leave` instance, given the validated data.
+            """
+            return Leave.objects.create(**validated_data)
+
+        def update(self, instance, validated_data):
+            """
+            Update and return an existing `Leave` instance, given the validated data.
+            """
+            instance.leave_requester = validated_data.get('leave_requester', instance.leave_requester)
+            instance.start_date = validated_data.get('start_date', instance.start_date)
+            instance.end_date = validated_data.get('end_date', instance.end_date)
+            instance.reason = validated_data.get('reason', instance.reason)
+            instance.status = validated_data.get('status', instance.status)
+            instance.updated_at = validated_data.get('updated_at', instance.updated_at)
+            instance.save()
+            return instance
--- leave/submission_app/tests.py
+++ leave/submission_app/tests.py
@@ -1,19 +1,74 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.urls import reverse
+from .models import Leave
 
-class LeaveRequestTestCase(TestCase):
-    def test_list_leave_requests(self):
-        response = self.client.get('/leave/')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'leave/leave_list.html')
+class LeaveModelTestCase(TestCase):
+    '''
+    Test module for Leave model
 
-    def test_submit_leave_request(self):
-        response = self.client.get('/leave/submit/')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'leave/leave_submit.html')
+    '''
+    def setUp(self):
+        self.leave = Leave.objects.create(
+            leave_requester='John Doe',
+            start_date='2020-01-01',
+            end_date='2020-01-01',
+            reason='I need a vacation',
+            status='P'
+        )
 
-    def test_edit_exsiting_leave_request(self):
-        response = self.client.get('/leave/edit/1/')
+    def test_leave_model_can_create_a_leave(self):
+        self.assertIsInstance(self.leave, Leave)
+
+    def test_leave_model_can_create_a_leave_with_a_requester(self):
+        self.assertEqual(self.leave.leave_requester, 'John Doe')
+
+    def test_edit_leave_request(self):
+        self.leave.leave_requester = 'Jane Doe'
+        self.leave.save()
+        self.assertEqual(self.leave.leave_requester, 'Jane Doe')
+
+    def test_delete_leave_request(self):
+        self.leave.delete()
+        self.assertEqual(self.leave.leave_requester, '')
+
+class LeaveRequestAPITestCase(TestCase):
+    """Test suite for the api views."""
+
+    def setUp(self):
+        self.client = APIClient()
+        self.leave_request = {
+            'leave_requester': 'John Doe',
+            'start_date': '2020-01-01',
+            'end_date': '2020-01-01',
+            'reason': 'I need a vacation',
+            'status': 'P'
+        }
+        self.client.post(self.leave_request, format=json)
+
+    def test_api_can_create_a_leave_request(self):
+        """Test the api has leave creation capability."""
+        response = self.client.post('/leave/', self.leave_request, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_api_can_get_a_leave_request(self):
+        """Test the api can get a given leave."""
+        leave = Leave.objects.get()
+        response = self.client.get(f'/leave/{leave.id}/')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'leave/leave_edit.html')
+        self.assertContains(response, leave)
+
+    def test_api_can_update_leave_request(self):
+        leave = Leave.objects.get()
+        change_leave = {'leave_requester': 'Jane Doe'}
+        res = self.client.put(f'/leave/{leave.id}/', change_leave, format='json')
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_api_can_delete_leave_request(self):
+        leave = Leave.objects.get()
+        response = self.client.delete(f'/leave/{leave.id}/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
 
 
